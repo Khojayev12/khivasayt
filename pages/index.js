02866bee18143a48de636b6be173f76e33cfec55
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-04-08 13:40:48 +0500

    change language

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,21 @@ import { Layout } from '../components/Layout'
 import styles from '../styles/Home.module.css'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const { t } = useTranslation();
+
+  const router = useRouter();
+
+  const changeLang = () => {
+    router.push(router.asPath, router.asPath, { locale: 'en' });
+  }
+
   return (
     <Layout className={styles.container}>
      <h1> Hello World!</h1>
+     <button onClick={changeLang}>Change</button>
      <h2>{t('hi')}</h2>
     </Layout>
   )
