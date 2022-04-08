3abde3bd07058af987ad88896b3e5d21a17a2b15
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-04-08 13:38:56 +0500

    min lang

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,12 @@ import { Layout } from '../components/Layout'
 import styles from '../styles/Home.module.css'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
 
 export default function Home() {
   const { t } = useTranslation();
-  const router = useRouter();
-  const handle = () => {
-    router.pathname = 'uz';
-  }
   return (
     <Layout className={styles.container}>
      <h1> Hello World!</h1>
-     <button onClick={handle}>Change</button>
      <h2>{t('hi')}</h2>
     </Layout>
   )
